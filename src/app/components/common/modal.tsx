f5af11948378bada8807d928693b6cd540865f06
Ademola Onasoga <ademola.onasoga@gmail.com>
2024-11-30 10:37:03 +0100

    fix react

--- src/app/components/common/modal.tsx
+++ src/app/components/common/modal.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'React';
+import { ReactElement } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
